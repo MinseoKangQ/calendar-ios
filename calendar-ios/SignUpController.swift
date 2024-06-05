3bb0ef79e9a9647c6e65e0e252f4afe20ebd1719
MinseoKangQ <kms02171@naver.com>
2024-06-06 02:30:46 +0900

    :lipstick: Custom 색상 Assets에 정의

--- calendar-ios/SignUpController.swift
+++ calendar-ios/SignUpController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-// blue : 007aff
-// placeholder default : c7c7cd
-// red : ff3b30
+
 
 // pw : Aa1!aaaa
 
@@ -17,7 +15,6 @@ import UIKit
 // TODO: 2) 모든 필드가 파란색이면 회원가입 버튼 활성화
 // TODO: 3) 회원가입 누르면 "회원가입이 완료되었습니다! 로그인" 화면으로 넘어가기
 // TODO: 4) 홈으로 누르면 이전 화면으로 넘어가기
-// TODO: 5) textField가 아닌 다른 곳 누르면 키보드 사라지게 하기
 
 class SignUpController: UIViewController {
 
@@ -36,6 +33,10 @@ class SignUpController: UIViewController {
     
     var isShowKeyboard = false
     
+    let CUSTOM_BLUE = UIColor(named: "CustomBlue") // 007aff
+    let CUSTOM_GREY = UIColor(named: "CustomGrey") // c7c7cd
+    let CUSTOM_RED = UIColor(named: "CustomRed") // ff3b30
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -58,15 +59,15 @@ class SignUpController: UIViewController {
         
         // TODO: 1) (400) 이메일 형식이 맞지 않다면
         // - emailLabel "이메일 형식을 맞춰주세요."
-        // - emailTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor -> 빨간색
+        // - emailTextField.layer.borderColor = CUSTOM_RED?.cgColor -> 빨간색
         
         // TODO: 2) (409) 이메일 형식이 맞는데 중복되었다면
         // - emailLabel "사용할 수 없는 이메일입니다."
-        // - emailTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor -> 빨간색
+        // - emailTextField.layer.borderColor = CUSTOM_RED?.cgColor -> 빨간색
         
         // TODO: 3) (200) 사용 가능한 이메일이라면
         // - emailLabel "사용 가능한 이메일입니다."
-        // - emailTextField.layer.borderColor = UIColor(hexCode: "007aff").cgColor -> 파란색
+        // - emailTextField.layer.borderColor = CUSTOM_BLUE?.cgColor -> 파란색
     }
     
     @IBAction func idEditingChanged(_ sender: UITextField) {
@@ -74,15 +75,15 @@ class SignUpController: UIViewController {
         
         // TODO: 1) (400) 영소문자 + 숫자 조합 7자 이상이 아니라면
         // - idLabel "아이디는 영소문자 + 숫자 조합으로 7자 이상이어야 합니다."
-        // - idTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor -> 빨간색
+        // - idTextField.layer.borderColor = CUSTOM_RED?.cgColor -> 빨간색
         
         // TODO: 2) (409) 중복된 아이디라면
         // - idLabel "이미 사용중인 아이디입니다."
-        // - idTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor -> 빨간색
+        // - idTextField.layer.borderColor = CUSTOM_RED?.cgColor -> 빨간색
         
         // TODO: 3) (200) 사용 가능한 이메일이라면
         // - idLabel "사용 가능한 아이디입니다."
-        // - idTextField.layer.borderColor = UIColor(hexCode: "007aff").cgColor -> 파란색
+        // - idTextField.layer.borderColor = CUSTOM_BLUE?.cgColor -> 파란색
         
     }
     
@@ -94,13 +95,13 @@ class SignUpController: UIViewController {
             if isPasswordValid(password) {
                 // 사용 가능한 비밀번호라면
                 pwLabel.text = "사용 가능한 비밀번호입니다."
-                pwLabel.textColor = UIColor(hexCode: "007aff")
-                pwTextField.layer.borderColor = UIColor(hexCode: "007aff").cgColor
+                pwLabel.textColor = CUSTOM_BLUE
+                pwTextField.layer.borderColor = CUSTOM_BLUE?.cgColor
             } else {
                 // 영대문자 + 특수문자 + 숫자 + 영소문자 조합에 맞지 않다면
                 pwLabel.text = "비밀번호는 영대문자 + 특수문자 + 숫자 + 영소문자 조합이어야 합니다."
-                pwLabel.textColor = UIColor(hexCode: "ff3b30")
-                pwTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor
+                pwLabel.textColor = CUSTOM_RED
+                pwTextField.layer.borderColor = CUSTOM_RED?.cgColor
             }
         }
     }
@@ -115,19 +116,19 @@ class SignUpController: UIViewController {
                 if isPasswordValid(pwCheckText) {
                     // 비밀번호가 일치하고 조건을 만족하는 경우
                     pwCheckLabel.text = "비밀번호가 일치합니다."
-                    pwCheckLabel.textColor = UIColor(hexCode: "007aff")
-                    pwCheckTextField.layer.borderColor = UIColor(hexCode: "007aff").cgColor
+                    pwCheckLabel.textColor = CUSTOM_BLUE
+                    pwCheckTextField.layer.borderColor = CUSTOM_BLUE?.cgColor
                 } else {
                     // 비밀번호가 일치하지만 조건을 만족하지 않는 경우
                     pwCheckLabel.text = "비밀번호는 영대문자 + 특수문자 + 숫자 + 영소문자 조합이어야 합니다."
-                    pwCheckLabel.textColor = UIColor(hexCode: "ff3b30")
-                    pwCheckTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor
+                    pwCheckLabel.textColor = CUSTOM_RED
+                    pwCheckTextField.layer.borderColor = CUSTOM_RED?.cgColor
                 }
             } else {
                 // 비밀번호가 일치하지 않는 경우
                 pwCheckLabel.text = "비밀번호가 일치하지 않습니다."
-                pwCheckLabel.textColor = UIColor(hexCode: "ff3b30")
-                pwCheckTextField.layer.borderColor = UIColor(hexCode: "ff3b30").cgColor
+                pwCheckLabel.textColor = CUSTOM_RED
+                pwCheckTextField.layer.borderColor = CUSTOM_RED?.cgColor
             }
         }
     }
@@ -172,37 +173,37 @@ class SignUpController: UIViewController {
     private func setupUI() {
         emailTextField.layer.cornerRadius = 14
         emailTextField.layer.borderWidth = 1
-        emailTextField.layer.borderColor = UIColor(hexCode: "c7c7cd").cgColor
+        emailTextField.layer.borderColor = CUSTOM_GREY?.cgColor
         emailTextField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
         emailTextField.leftViewMode = .always
         
         idTextField.layer.cornerRadius = 14
         idTextField.layer.borderWidth = 1
-        idTextField.layer.borderColor = UIColor(hexCode: "c7c7cd").cgColor
+        idTextField.layer.borderColor = CUSTOM_GREY?.cgColor
         idTextField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
         idTextField.leftViewMode = .always
         
         pwTextField.layer.cornerRadius = 14
         pwTextField.layer.borderWidth = 1
-        pwTextField.layer.borderColor = UIColor(hexCode: "c7c7cd").cgColor
+        pwTextField.layer.borderColor = CUSTOM_GREY?.cgColor
         pwTextField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
         pwTextField.leftViewMode = .always
         
         pwCheckTextField.layer.cornerRadius = 14
         pwCheckTextField.layer.borderWidth = 1
-        pwCheckTextField.layer.borderColor = UIColor(hexCode: "c7c7cd").cgColor
+        pwCheckTextField.layer.borderColor = CUSTOM_GREY?.cgColor
         pwCheckTextField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
         pwCheckTextField.leftViewMode = .always
         
         // 버튼
         signUpBtn.layer.cornerRadius = 14
         signUpBtn.layer.borderWidth = 1
-        signUpBtn.backgroundColor = UIColor(hexCode: "007aff")
-        signUpBtn.layer.borderColor = UIColor(hexCode: "007aff").cgColor
+        signUpBtn.backgroundColor = CUSTOM_BLUE
+        signUpBtn.layer.borderColor = CUSTOM_BLUE?.cgColor
         
         homeBtn.layer.cornerRadius = 14
         homeBtn.layer.borderWidth = 1
-        homeBtn.layer.borderColor = UIColor(hexCode: "007aff").cgColor
+        homeBtn.layer.borderColor = CUSTOM_BLUE?.cgColor
         
         // ===== 비밀번호 관련 필드 =====
         pwTextField.isSecureTextEntry = true
@@ -211,26 +212,4 @@ class SignUpController: UIViewController {
         pwCheckTextField.textContentType = .oneTimeCode
     }
 
-}
-
-// Hex
-extension UIColor {
-    
-    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
-        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        
-        if hexFormatted.hasPrefix("#") {
-            hexFormatted = String(hexFormatted.dropFirst())
-        }
-        
-        assert(hexFormatted.count == 6, "Invalid hex code used.")
-        
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
-        
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
-    }
 }
