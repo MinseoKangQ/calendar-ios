56a983dc7910c0c3e92376c2226e998187ed50db
MinseoKangQ <kms02171@naver.com>
2024-06-09 23:40:43 +0900

    :fire: 필요없는 코드 삭제

--- calendar-ios/CustomTableViewCell.swift
+++ calendar-ios/CustomTableViewCell.swift
@@ -20,43 +20,5 @@ class CustomTableViewCell: UITableViewCell {
 //        setupViews()
 //        setupConstraints()
     }
-    
-    private func setupViews() {
 
-        checkbox = M13Checkbox()
-        contentView.addSubview(checkbox)
-        
-        // Initialize the labels
-        titleLabel = UILabel()
-        titleLabel.font = UIFont.systemFont(ofSize: 17)
-        contentView.addSubview(titleLabel)
-        
-        subtitleLabel = UILabel()
-        subtitleLabel.font = UIFont.systemFont(ofSize: 15)
-        subtitleLabel.textColor = .gray
-        contentView.addSubview(subtitleLabel)
-    }
-    
-    private func setupConstraints() {
-        checkbox.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            // Checkbox constraints
-            checkbox.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
-            checkbox.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            
-            // Title label constraints
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: checkbox.leadingAnchor, constant: -10),
-            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            
-            // Subtitle label constraints
-            subtitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            subtitleLabel.trailingAnchor.constraint(equalTo: checkbox.leadingAnchor, constant: -10),
-            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
-            subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
-        ])
-    }
 }
