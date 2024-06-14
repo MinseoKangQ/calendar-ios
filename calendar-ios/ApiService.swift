a07a2376c5a964bf1c680793548f9459ebb1a48d
MinseoKangQ <kms02171@naver.com>
2024-06-15 03:05:23 +0900

    :sparkles: 회원탈퇴 API 연동

--- calendar-ios/ApiService.swift
+++ calendar-ios/ApiService.swift
@@ -9,12 +9,12 @@ import Foundation
 
 class ApiService {
     
-//    static let BASE_URL = "http://172.30.1.13:8080"
     static let BASE_URL = "http://localhost:8080"
-    static let TMP_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrbXMwMjE3MSIsInJvbGVzIjoiVVNFUiIsImlhdCI6MTcxODI5NDQyNiwiZXhwIjoxNzE4ODk5MjI2fQ.nbExlpvvhSxBUv4hDoBUFDwlNFx87IULuN1-hrnu14k"
+    static let TMP_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrbXMwMjE3MSIsInJvbGVzIjoiVVNFUiIsImlhdCI6MTcxODM4ODI3NCwiZXhwIjoxNzE4OTkzMDc0fQ.zE8DTKuwXfTE3MQg2MCkxmKkyL4t4b7AscLRYTnsMgg"
     static var authToken: String?
     
-    // 이메일 중복 확인
+    // 이메일 중복 확인 API
+    // [GET] http://localhost:8080/api/users/email/{email}
     static func checkEmailDuplicated(email: String, completion: @escaping (Int) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/users/email/\(email)")!
         var request = URLRequest(url: url)
@@ -32,7 +32,8 @@ class ApiService {
         task.resume()
     }
     
-    // 아이디 중복 확인
+    // 아이디 중복 확인 API
+    // [GET] http://localhost:8080/api/users/userId/{userId}
     static func checkUserIdDuplicated(userId: String, completion: @escaping (Int) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/users/userId/\(userId)")!
         var request = URLRequest(url: url)
@@ -50,7 +51,8 @@ class ApiService {
         task.resume()
     }
     
-    // 회원가입
+    // 회원가입 API
+    // [POST] http://localhost:8080/api/users/signup
     static func signUp(userId: String, email: String, password: String, completion: @escaping (Int) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/users/signup")!
         var request = URLRequest(url: url)
@@ -83,7 +85,8 @@ class ApiService {
         task.resume()
     }
     
-    // 로그인
+    // 로그인 API
+    // [POST] http://localhost:8080/api/users/signin
     static func signIn(userId: String, password: String, completion: @escaping (Int) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/users/signin")!
         var request = URLRequest(url: url)
@@ -121,7 +124,8 @@ class ApiService {
         task.resume()
     }
     
-    // 할 일 등록
+    // 할 일 등록 API
+    // [POST] http://localhost:8080/api/todo
     static func addTodo(date: String, title: String, category: String, completion: @escaping (Bool) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/todo")!
         var request = URLRequest(url: url)
@@ -168,23 +172,19 @@ class ApiService {
     }
     
     
-    // 특정 날짜의 할 일 목록 가져오기
+    // 특정 날짜의 할 일 목록 가져오기 API
+    // [GET] http://localhost:8080/api/todo/oneDay/{date}
     static func getTodoList(for date: String, completion: @escaping ([TodoItem]?) -> Void) {
-        print("할 일 목록 가져오기")
-        print("\(date)")
         
         // 날짜 포맷 확인 및 변환
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         guard let formattedDate = dateFormatter.date(from: date) else {
-            print("Invalid date format: \(date)")
             completion(nil)
             return
         }
         
         let formattedDateString = dateFormatter.string(from: formattedDate)
-        print("Formatted Date: \(formattedDateString)")
-        
         
         guard let url = URL(string: "\(BASE_URL)/api/todo/oneDay/\(date)") else {
             completion(nil)
@@ -226,6 +226,7 @@ class ApiService {
     }
     
     // 할 일 체크/체크 해제 API 호출
+    // [PUT] http://localhost:8080/api/todo/category/{todoId}
     static func toggleTodoCheck(todoId: Int, completion: @escaping (Bool) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/todo/checking/\(todoId)")!
         var request = URLRequest(url: url)
@@ -256,7 +257,8 @@ class ApiService {
         task.resume()
     }
     
-    // 할 일 수정
+    // 할 일 수정 API
+    // [PUT] http://localhost:8080/api/todo/title/{todoId}
     static func updateTodoTitle(todoId: Int, title: String, completion: @escaping (Bool) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/todo/title/\(todoId)")!
         var request = URLRequest(url: url)
@@ -301,6 +303,7 @@ class ApiService {
     }
     
     // 할 일 삭제 API
+    // [DELETE] http://localhost:8080/api/todo/{todoId}
     static func deleteTodo(todoId: Int, completion: @escaping (Bool) -> Void) {
         let url = URL(string: "\(BASE_URL)/api/todo/\(todoId)")!
         var request = URLRequest(url: url)
@@ -332,6 +335,7 @@ class ApiService {
     }
     
     // 한 달 조회 API
+    // [GET] http://localhost:8080/api/todo/oneMonth/{month}
     static func getOneMonthTodoList(for month: String, completion: @escaping (TodoResponse?) -> Void) {
         guard let url = URL(string: "\(BASE_URL)/api/todo/oneMonth/\(month)") else {
             completion(nil)
@@ -373,6 +377,7 @@ class ApiService {
     }
     
     // 아직 못끝낸 할 일 개수 조회 API
+    // [GET] http://localhost:8080/api/todo/notDoneCount
     static func getNotDoneCount(completion: @escaping (NotDoneCountReponse?) -> Void) {
         guard let url = URL(string: "\(BASE_URL)/api/todo/notDoneCount") else {
             completion(nil)
@@ -407,6 +412,38 @@ class ApiService {
             } catch {
                 print("Failed to decode response: \(error.localizedDescription)")
                 completion(nil)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // 회원 탈퇴 API
+    // [DELETE] http://localhost:8080/api/users
+    static func deleteUser(completion: @escaping (Bool) -> Void) {
+        let url = URL(string: "\(BASE_URL)/api/users")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let token = authToken {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        } else {
+            request.setValue("Bearer \(TMP_TOKEN)", forHTTPHeaderField: "Authorization")
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("요청 실패: \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                completion(true)
+            } else {
+                print("예상치 못한 코드를 받음: \((response as? HTTPURLResponse)?.statusCode ?? -1)")
+                completion(false)
             }
         }
         
--- calendar-ios/SettingsViewController.swift
+++ calendar-ios/SettingsViewController.swift
@@ -54,16 +54,86 @@ extension SettingsViewController: UITableViewDelegate {
         switch indexPath.row {
         case 0:
             // Handle 로그아웃 action
-            print("로그아웃 selected")
-            // Add your logout logic here
+            showLogoutConfirmation()
             break
         case 1:
             // Handle 회원탈퇴 action
             print("회원탈퇴 selected")
-            // Add your delete account logic here
-            break
+            // Handle 회원탈퇴 action
+             showDeleteAccountConfirmation()
         default:
             break
+        }
+    }
+    
+    func showLogoutConfirmation() {
+        let alertController = UIAlertController(title: "로그아웃", message: "정말 로그아웃 하시겠습니까?", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "예", style: .destructive) { _ in
+            self.logout()
+        }
+        
+        let cancelAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func logout() {
+        ApiService.authToken = nil
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        loginViewController.modalPresentationStyle = .fullScreen
+        present(loginViewController, animated: true, completion: nil)
+    }
+
+    func showDeleteAccountConfirmation() {
+        let alertController = UIAlertController(title: "회원탈퇴", message: "정말로 탈퇴하시겠습니까? 탈퇴하시면 모든 정보가 사라집니다. 동의하시면 아래에 \"동의합니다\" 라고 입력해주세요", preferredStyle: .alert)
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "동의합니다"
+        }
+        
+        let confirmAction = UIAlertAction(title: "네", style: .destructive) { _ in
+            if let input = alertController.textFields?.first?.text, input == "동의합니다" {
+                self.deleteAccount()
+            } else {
+                let errorAlert = UIAlertController(title: "오류", message: "입력된 내용이 올바르지 않습니다.", preferredStyle: .alert)
+                errorAlert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                self.present(errorAlert, animated: true, completion: nil)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
+    func deleteAccount() {
+        ApiService.deleteUser { success in
+            DispatchQueue.main.async {
+                if success {
+                    let successAlert = UIAlertController(title: "회원탈퇴", message: "회원탈퇴가 완료되었습니다!", preferredStyle: .alert)
+                    successAlert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
+                        ApiService.authToken = nil
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+                        loginViewController.modalPresentationStyle = .fullScreen
+                        self.present(loginViewController, animated: true, completion: nil)
+                    })
+                    self.present(successAlert, animated: true, completion: nil)
+                } else {
+                    let errorAlert = UIAlertController(title: "오류", message: "회원탈퇴에 실패했습니다. 다시 시도해주세요.", preferredStyle: .alert)
+                    errorAlert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+                    self.present(errorAlert, animated: true, completion: nil)
+                }
+            }
         }
     }
 }
