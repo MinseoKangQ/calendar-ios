6ab4648d2f7729465aa66adde6d0949dc9bd4899
MinseoKangQ <kms02171@naver.com>
2024-06-15 16:56:48 +0900

    :recycle: SettingsViewController 리팩토링

--- calendar-ios/SettingsViewController.swift
+++ calendar-ios/SettingsViewController.swift
@@ -20,6 +20,7 @@ class SettingsViewController: UIViewController {
         updateDarkMode()
     }
     
+    // 다크 모드 업데이트
     func updateDarkMode() {
         let darkModeEnabled = darkModeSwitch.isOn
         view.window?.overrideUserInterfaceStyle = darkModeEnabled ? .dark : .light
@@ -27,43 +28,47 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        appSettingsTableView.dataSource = self
-        appSettingsTableView.delegate = self
-        
-        modeView.layer.cornerRadius = 5
-        modeView.layer.masksToBounds = true
-        
+        setupSettingsTableView()
+        setupModeView()
+        setupDarkModeSwitch()
+    }
+    
+    // TableView 설정
+    func setupSettingsTableView() {
+        appSettingsTableView.dataSource = self // dataSource 등록
+        appSettingsTableView.delegate = self // delegate 등록
         appSettingsTableView.layer.cornerRadius = 5
         appSettingsTableView.layer.masksToBounds = true
-
-        // 다크모드
+    }
+    
+    // 모드 뷰 UI 설정
+    func setupModeView() {
+        modeView.layer.cornerRadius = 5
+        modeView.layer.masksToBounds = true
+    }
+    
+    // 다크 모드 스위치 설정
+    func setupDarkModeSwitch() {
         darkModeSwitch.isOn = UserDefaults.standard.bool(forKey: "darkModeEnabled")
         updateDarkMode()
         darkModeSwitch.addTarget(self, action: #selector(darkModeSwitchChanged(_:)), for: .valueChanged)
-        
     }
     
+    // 다크 모드 스위치 변경 시 호출
     @objc func darkModeSwitchChanged(_ sender: UISwitch) {
         UserDefaults.standard.set(sender.isOn, forKey: "darkModeEnabled")
         updateDarkMode()
     }
-    
-//    func updateDarkMode() {
-//        if darkModeSwitch.isOn {
-//            view.window?.overrideUserInterfaceStyle = .dark
-//        } else {
-//            view.window?.overrideUserInterfaceStyle = .light
-//        }
-//    }
 }
 
 extension SettingsViewController: UITableViewDataSource {
     
+    // TableView 행 개수 반환
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return appSettingsData.count
     }
     
+    // 셀 구성
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
         cell.textLabel?.text = appSettingsData[indexPath.row]
@@ -72,28 +77,24 @@ extension SettingsViewController: UITableViewDataSource {
     }
 }
 
-// MARK: - UITableViewDelegate
-
 extension SettingsViewController: UITableViewDelegate {
     
+    // 행 선택 시 호출
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
         switch indexPath.row {
-        case 0:
-            // Handle 로그아웃 action
+        case 0: // Handle 로그아웃 action
             showLogoutConfirmation()
             break
-        case 1:
-            // Handle 회원탈퇴 action
-            print("회원탈퇴 selected")
-            // Handle 회원탈퇴 action
+        case 1: // Handle 회원탈퇴 action
              showDeleteAccountConfirmation()
         default:
             break
         }
     }
     
+    // 로그아웃 확인 창 표시
     func showLogoutConfirmation() {
         let alertController = UIAlertController(title: "로그아웃", message: "정말 로그아웃 하시겠습니까?", preferredStyle: .alert)
         
@@ -109,7 +110,9 @@ extension SettingsViewController: UITableViewDelegate {
         present(alertController, animated: true, completion: nil)
     }
 
+    // 로그아웃 처리
     func logout() {
+        // 헤더 값 삭제
         ApiService.authToken = nil
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -118,6 +121,7 @@ extension SettingsViewController: UITableViewDelegate {
         present(loginViewController, animated: true, completion: nil)
     }
 
+    // 회원탈퇴 확인 창 표시
     func showDeleteAccountConfirmation() {
         let alertController = UIAlertController(title: "회원탈퇴", message: "정말로 탈퇴하시겠습니까? 탈퇴하시면 모든 정보가 사라집니다. 동의하시면 아래에 \"동의합니다\" 라고 입력해주세요", preferredStyle: .alert)
         
@@ -129,9 +133,7 @@ extension SettingsViewController: UITableViewDelegate {
             if let input = alertController.textFields?.first?.text, input == "동의합니다" {
                 self.deleteAccount()
             } else {
-                let errorAlert = UIAlertController(title: "오류", message: "입력된 내용이 올바르지 않습니다.", preferredStyle: .alert)
-                errorAlert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
-                self.present(errorAlert, animated: true, completion: nil)
+                self.showErrorAlert(message: "입력된 내용이 올바르지 않습니다.")
             }
         }
         
@@ -143,25 +145,41 @@ extension SettingsViewController: UITableViewDelegate {
         present(alertController, animated: true, completion: nil)
     }
 
+    // 회원탈퇴 처리
     func deleteAccount() {
-        ApiService.deleteUser { success in
-            DispatchQueue.main.async {
-                if success {
-                    let successAlert = UIAlertController(title: "회원탈퇴", message: "회원탈퇴가 완료되었습니다!", preferredStyle: .alert)
-                    successAlert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
-                        ApiService.authToken = nil
-                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                        let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
-                        loginViewController.modalPresentationStyle = .fullScreen
-                        self.present(loginViewController, animated: true, completion: nil)
-                    })
-                    self.present(successAlert, animated: true, completion: nil)
-                } else {
-                    let errorAlert = UIAlertController(title: "오류", message: "회원탈퇴에 실패했습니다. 다시 시도해주세요.", preferredStyle: .alert)
-                    errorAlert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
-                    self.present(errorAlert, animated: true, completion: nil)
+        func deleteAccount() {
+           
+            // 회원탈퇴 API 호출
+            ApiService.deleteUser { success in
+                DispatchQueue.main.async {
+                    if success {
+                        self.showSuccessAlert()
+                    } else {
+                        self.showErrorAlert(message: "회원탈퇴에 실패했습니다. 다시 시도해주세요.")
+                    }
                 }
             }
         }
+    }
+    
+    // 성공 알림창 표시
+    func showSuccessAlert() {
+        let successAlert = UIAlertController(title: "회원탈퇴", message: "회원탈퇴가 완료되었습니다!", preferredStyle: .alert)
+        successAlert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
+            // 헤더 값 삭제
+            ApiService.authToken = nil
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+            loginViewController.modalPresentationStyle = .fullScreen
+            self.present(loginViewController, animated: true, completion: nil)
+        })
+        present(successAlert, animated: true, completion: nil)
+    }
+
+    // 오류 알림창 표시
+    func showErrorAlert(message: String) {
+        let errorAlert = UIAlertController(title: "오류", message: message, preferredStyle: .alert)
+        errorAlert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        present(errorAlert, animated: true, completion: nil)
     }
 }
