328aef3ddfee02769f4aa1434742f9d9c6c89007
MinseoKangQ <kms02171@naver.com>
2024-06-15 17:37:54 +0900

    :recycle: CustomTableViewController 리팩토링

--- calendar-ios/CustomTableViewCell.swift
+++ calendar-ios/CustomTableViewCell.swift
@@ -10,9 +10,16 @@ import M13Checkbox
 
 class CustomTableViewCell: UITableViewCell {
     
-    weak var delegate: CustomTableViewCellDelegate?
-    
+    // Outlets
     @IBOutlet weak var checkBox: M13Checkbox!
+    @IBOutlet weak var labelView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBAction func checkBoxValueChanged(_ sender: M13Checkbox) {
+    }
+    
+    weak var delegate: CustomTableViewCellDelegate?
     
     let LABEL_RED = UIColor(named: "CategoryRedLabel")
     let LABEL_BLUE = UIColor(named: "CategoryBlueLabel")
@@ -24,14 +31,6 @@ class CustomTableViewCell: UITableViewCell {
     let VIEW_PURPLE = UIColor(named: "TodoPurpleBackground")
     let VIEW_YELLOW = UIColor(named: "TodoYellowBackground")
     
-    @IBOutlet weak var labelView: UIView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var categoryLabel: UILabel!
-    
-    @IBAction func checkBoxValueChanged(_ sender: M13Checkbox) {
-        print("터치됨")
-    
-    }
     override func awakeFromNib() {
         super.awakeFromNib()
         setupCheckbox()
@@ -45,17 +44,19 @@ class CustomTableViewCell: UITableViewCell {
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 8, right: 0))
     }
     
+    // 제목 레이블에 TapGesture 추가
     private func addTapGestureToTitleLabel() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(titleLabelTapped))
         titleLabel.isUserInteractionEnabled = true
         titleLabel.addGestureRecognizer(tapGesture)
     }
     
+    // 제목 레이블 탭 처리
     @objc private func titleLabelTapped() {
         delegate?.titleLabelTapped(in: self, with: titleLabel.text ?? "")
     }
     
-    // 카테고리에 따라 view, label 색상 변경
+    // 카테고리에 따라 View와 Label 색상 변경
     func configureBackgroundColor(category: String) {
         switch category {
         case "IMPORTANT":
@@ -93,6 +94,7 @@ class CustomTableViewCell: UITableViewCell {
         checkBox.stateChangeAnimation = .bounce(.fill) // 애니메이션
     }
     
+    // 셀 하이라이트 비활성화
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {
     }
 
